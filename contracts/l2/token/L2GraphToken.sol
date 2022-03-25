8db7cd3c888feeae760fb06a7854546c4acb1a5b
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-03-25 17:03:43 -0300

    Hmm copying the GRT contract is ugly, but can't find a cleaner way yet

--- contracts/l2/token/L2GraphToken.sol
+++ contracts/l2/token/L2GraphToken.sol
@@ -2,26 +2,238 @@
 
 pragma solidity ^0.7.6;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "../../upgrades/GraphUpgradeable.sol";
 import "../../token/GraphToken.sol";
 import "../../arbitrum/IArbToken.sol";
+import "../../governance/Governed.sol";
+
+/**
+ * @title GraphTokenUpgradeable contract
+ * @dev This is the implementation of the ERC20 Graph Token.
+ * The implementation exposes a Permit() function to allow for a spender to send a signed message
+ * and approve funds to a spender following EIP2612 to make integration with other contracts easier.
+ *
+ * The token is initially owned by the deployer address that can mint tokens to create the initial
+ * distribution. For convenience, an initial supply can be passed in the constructor that will be
+ * assigned to the deployer.
+ *
+ * The governor can add contracts allowed to mint indexing rewards.
+ *
+ * Note this is an exact copy of the original GraphToken contract, but using
+ * initializer functions and upgradeable OpenZeppelin contracts instead of
+ * the original's constructor + non-upgradeable approach.
+ */
+contract GraphTokenUpgradeable is GraphUpgradeable, Governed, ERC20Upgradeable, ERC20BurnableUpgradeable {
+    using SafeMath for uint256;
+
+    // -- EIP712 --
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#definition-of-domainseparator
+
+    bytes32 private constant DOMAIN_TYPE_HASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)"
+        );
+    bytes32 private constant DOMAIN_NAME_HASH = keccak256("Graph Token");
+    bytes32 private constant DOMAIN_VERSION_HASH = keccak256("0");
+    bytes32 private constant DOMAIN_SALT =
+        0x51f3d585afe6dfeb2af01bba0889a36c1db03beec88c6a4d0c53817069026afa; // Randomly generated salt
+    bytes32 private constant PERMIT_TYPEHASH =
+        keccak256(
+            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+        );
+
+    // -- State --
+
+    bytes32 private DOMAIN_SEPARATOR;
+    mapping(address => bool) private _minters;
+    mapping(address => uint256) public nonces;
+
+    // -- Events --
+
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+
+    modifier onlyMinter() {
+        require(isMinter(msg.sender), "Only minter can call");
+        _;
+    }
+
+    /**
+     * @dev Graph Token Contract initializer.
+     * @param _initialSupply Initial supply of GRT
+     */
+    function _initialize(uint256 _initialSupply) internal {
+        __ERC20_init("Graph Token", "GRT");
+        Governed._initialize(msg.sender);
+
+        // The Governor has the initial supply of tokens
+        _mint(msg.sender, _initialSupply);
+
+        // The Governor is the default minter
+        _addMinter(msg.sender);
+
+        // EIP-712 domain separator
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                DOMAIN_TYPE_HASH,
+                DOMAIN_NAME_HASH,
+                DOMAIN_VERSION_HASH,
+                _getChainID(),
+                address(this),
+                DOMAIN_SALT
+            )
+        );
+    }
+
+    /**
+     * @dev Approve token allowance by validating a message signed by the holder.
+     * @param _owner Address of the token holder
+     * @param _spender Address of the approved spender
+     * @param _value Amount of tokens to approve the spender
+     * @param _deadline Expiration time of the signed permit
+     * @param _v Signature version
+     * @param _r Signature r value
+     * @param _s Signature s value
+     */
+    function permit(
+        address _owner,
+        address _spender,
+        uint256 _value,
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(PERMIT_TYPEHASH, _owner, _spender, _value, nonces[_owner], _deadline)
+                )
+            )
+        );
+        nonces[_owner] = nonces[_owner].add(1);
+
+        address recoveredAddress = ECDSA.recover(digest, abi.encodePacked(_r, _s, _v));
+        require(_owner == recoveredAddress, "GRT: invalid permit");
+        require(_deadline == 0 || block.timestamp <= _deadline, "GRT: expired permit");
+
+        _approve(_owner, _spender, _value);
+    }
+
+    /**
+     * @dev Add a new minter.
+     * @param _account Address of the minter
+     */
+    function addMinter(address _account) external onlyGovernor {
+        _addMinter(_account);
+    }
+
+    /**
+     * @dev Remove a minter.
+     * @param _account Address of the minter
+     */
+    function removeMinter(address _account) external onlyGovernor {
+        _removeMinter(_account);
+    }
+
+    /**
+     * @dev Renounce to be a minter.
+     */
+    function renounceMinter() external {
+        _removeMinter(msg.sender);
+    }
+
+    /**
+     * @dev Mint new tokens.
+     * @param _to Address to send the newly minted tokens
+     * @param _amount Amount of tokens to mint
+     */
+    function mint(address _to, uint256 _amount) external onlyMinter {
+        _mint(_to, _amount);
+    }
+
+    /**
+     * @dev Return if the `_account` is a minter or not.
+     * @param _account Address to check
+     * @return True if the `_account` is minter
+     */
+    function isMinter(address _account) public view returns (bool) {
+        return _minters[_account];
+    }
+
+    /**
+     * @dev Add a new minter.
+     * @param _account Address of the minter
+     */
+    function _addMinter(address _account) private {
+        _minters[_account] = true;
+        emit MinterAdded(_account);
+    }
+
+    /**
+     * @dev Remove a minter.
+     * @param _account Address of the minter
+     */
+    function _removeMinter(address _account) private {
+        _minters[_account] = false;
+        emit MinterRemoved(_account);
+    }
+
+    /**
+     * @dev Get the running network chain ID.
+     * @return The chain ID
+     */
+    function _getChainID() private pure returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+}
 
 /**
  * @title L2 Graph Token Contract
  * @dev Provides the L2 version of the GRT token, meant to be minted/burned
  * through the L2GraphTokenGateway.
  */
-contract L2GraphToken is GraphUpgradeable, GraphToken, IArbToken {
+contract L2GraphToken is GraphTokenUpgradeable, IArbToken {
     using SafeMath for uint256;
+    address public gateway;
+    address public l1GRT;
 
+    event BridgeMinted(address indexed account, uint256 amount);
+    event BridgeBurned(address indexed account, uint256 amount);
+    event GatewaySet(address gateway);
+    event L1AddressSet(address l1Address);
+
+    modifier onlyGateway() {
+        require(msg.sender == gateway, "NOT_GATEWAY");
+        _;
+    }
     /**
      * @dev L2 Graph Token Contract Constructor.
      * @param _initialSupply Initial supply of GRT
      */
-    constructor(uint256 _initialSupply) GraphToken(_initialSupply) {
+    function initialize(uint256 _initialSupply) external onlyImpl {
+        GraphTokenUpgradeable._initialize(_initialSupply);
+    }
 
+    function setGateway(address gw) external onlyGovernor {
+        gateway = gw;
+        emit GatewaySet(gateway);
+    }
+
+    function setL1Address(address addr) external onlyGovernor {
+        l1GRT = addr;
+        emit L1AddressSet(l1GRT);
     }
 
     /**
@@ -29,8 +241,9 @@ contract L2GraphToken is GraphUpgradeable, GraphToken, IArbToken {
      * @param account Address to credit with the new tokens
      * @param amount Number of tokens to mint
      */
-    function bridgeMint(address account, uint256 amount) external override {
-        // TODO
+    function bridgeMint(address account, uint256 amount) external override onlyGateway {
+        _mint(account, amount);
+        emit BridgeMinted(account, amount);
     }
 
     /**
@@ -38,8 +251,9 @@ contract L2GraphToken is GraphUpgradeable, GraphToken, IArbToken {
      * @param account Address from which to extract the tokens
      * @param amount Number of tokens to burn
      */
-    function bridgeBurn(address account, uint256 amount) external override {
-        // TODO
+    function bridgeBurn(address account, uint256 amount) external override onlyGateway {
+        burnFrom(account, amount);
+        emit BridgeBurned(account, amount);
     }
 
     /**
@@ -47,7 +261,6 @@ contract L2GraphToken is GraphUpgradeable, GraphToken, IArbToken {
      * @return address of layer 1 token
      */
     function l1Address() external override view returns (address) {
-        // TODO
+        return l1GRT;
     }
-
 }

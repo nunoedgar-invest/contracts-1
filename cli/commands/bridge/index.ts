bbf495ca4af10909610a00c86b1120e4a3fef9bd
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-04-06 15:35:19 -0300

    Attempt to redeem L2 ticket if it fails, and add command to redeem

--- cli/commands/bridge/index.ts
+++ cli/commands/bridge/index.ts
@@ -1,6 +1,6 @@
 import yargs, { Argv } from 'yargs'
 
-import { sendToL2Command } from './to-l2'
+import { redeemSendToL2Command, sendToL2Command } from './to-l2'
 import { startSendToL1Command, finishSendToL1Command, waitFinishSendToL1Command } from './to-l1'
 import { cliOpts } from '../../defaults'
 
@@ -9,7 +9,8 @@ export const bridgeCommand = {
   describe: 'Graph token bridge actions.',
   builder: (yargs: Argv): yargs.Argv => {
     return yargs.option('-l', cliOpts.l2ProviderUrl)
-      .command(sendToL2Command).command(startSendToL1Command).command(finishSendToL1Command)
+      .command(sendToL2Command).command(redeemSendToL2Command)
+      .command(startSendToL1Command).command(finishSendToL1Command)
       .command(waitFinishSendToL1Command)
   },
   handler: (): void => {
